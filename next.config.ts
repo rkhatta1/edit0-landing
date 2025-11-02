a18fdb9788299a535375dde785faa5e35aa1c13c
rkhatta1 <rkhatta1@asu.edu>
2025-11-02 12:10:11 -0700

    Added the nojekyll file, just in case...

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/edit0-landing',
 };
 
 export default nextConfig;
