ea74e588e3dbfc164fcd9aa034efb4553f811522
rkhatta1 <rkhatta1@asu.edu>
2026-01-03 15:13:27 -0700

    [ci]: updated the workflow file to use the repo secrets.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  // basePath: '/edit0-landing',
+  basePath: '/edit0-landing',
 };
 
 export default nextConfig;
